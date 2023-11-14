5746be8853b4ebd999dc339ca919a5d5171cf20f
Aseem Baranwal <aseembaranwal23@gmail.com>
2023-11-13 19:50:55 -0500

    Added comments to the main.cpp

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ public:
 
     void PrintBooks(int bookID1, int bookID2) const;
 
-    void InsertBook(int bookID, const string& bookName, string authorName, bool availabilityStatus);
+    void InsertBook(int bookID, const string &bookName, string authorName, bool availabilityStatus);
 
     void BorrowBook(int patronID, int bookID, int patronPriority) const;
 
@@ -37,6 +37,7 @@ public:
 };
 
 int GatorLibrary::findSmallestGreater(vector<Node *> &books, int bookID) {
+    // Utility function used to find the closest book to the given bookID
     int l = 0, r = (int) books.size() - 1;
     while (l <= r) {
         int mid = l + (r - l) / 2;
@@ -50,6 +51,7 @@ int GatorLibrary::findSmallestGreater(vector<Node *> &books, int bookID) {
 }
 
 void GatorLibrary::PrintBook(int bookID) const {
+    // Print the book with the given bookID using the Red Black Tree printBook function
     Node *currBook = redBlackTree->search(bookID);
     if (currBook == nullptr) {
         cout << "Book " << bookID << " not found in the library" << endl << endl;
@@ -59,6 +61,7 @@ void GatorLibrary::PrintBook(int bookID) const {
 }
 
 void GatorLibrary::PrintBooks(int bookID1, int bookID2) const {
+    // Print all the books in the given range using the Red Black Tree printBooks function
     if (bookID1 > bookID2) {
         cout << "Invalid Input" << endl;
         return;
@@ -66,7 +69,8 @@ void GatorLibrary::PrintBooks(int bookID1, int bookID2) const {
     redBlackTree->printBooks(bookID1, bookID2);
 }
 
-void GatorLibrary::InsertBook(int bookID, const string& bookName, string authorName, bool availabilityStatus) {
+void GatorLibrary::InsertBook(int bookID, const string &bookName, string authorName, bool availabilityStatus) {
+    // Insert the book with the given details into a Red Black Tree as a node
     if (redBlackTree->search(bookID) != nullptr) {
         cout << "BookID already exists" << endl;
         return;
@@ -77,18 +81,23 @@ void GatorLibrary::InsertBook(int bookID, const string& bookName, string authorN
 }
 
 void GatorLibrary::BorrowBook(int patronID, int bookID, int patronPriority) const {
+    // Borrow the book with the given bookID by the patron with the given patronID
+    // Find if the requested book is present
     Node *bookNode = redBlackTree->search(bookID);
     if (bookNode == nullptr) {
         cout << "Book " << bookID << " not found in the library" << endl << endl;
         return;
     }
     if (bookNode->book->borrowerID == patronID) {
+        // Already borrowed by the patron, hence follow the testcase and say that it is reserved, but don't add to heap
         cout << "Book " << bookID << " Reserved by Patron " << patronID << endl << endl;
         return;
     }
     if (!bookNode->book->isAvailable) {
+        // Book is not available, hence add to the heap
+        // Also print the patrons whose reservations have been cancelled
         if (bookNode->minHeap->userIDToIndex.find(patronID) != bookNode->minHeap->userIDToIndex.end()) {
-            cout << "Patron already in the waitlist" << endl;
+            cout << "Patron already in the waitlist" << endl << endl;
             return;
         }
         bookNode->minHeap->insertReservation(new Reservation(patronID, patronPriority));
@@ -101,17 +110,21 @@ void GatorLibrary::BorrowBook(int patronID, int bookID, int patronPriority) cons
 }
 
 void GatorLibrary::ReturnBook(int patronID, int bookID) const {
+    // Return the book with the given bookID by the patron with the given patronID
     Node *bookNode = redBlackTree->search(bookID);
     if (bookNode == nullptr) {
+        // Book not found
         cout << "Book " << bookID << " not found in the library" << endl << endl;
         return;
     }
     if (bookNode->book->isAvailable) {
-        cout << "Book already available in the Library" << endl;
+        // Considering no duplicate books, this case will never occur
+        cout << "Book already available in the Library" << endl << endl;
         return;
     }
     if (bookNode->book->borrowerID != patronID) {
-        cout << "Patron did not borrow the book" << endl;
+        // Patron did not borrow the book
+        cout << "Patron did not borrow the book" << endl << endl;
         return;
     }
     bookNode->book->isAvailable = true;
@@ -127,19 +140,20 @@ void GatorLibrary::ReturnBook(int patronID, int bookID) const {
 }
 
 void GatorLibrary::DeleteBook(int bookID) {
+    // Delete the book with the given bookID from the Red Black Tree as a node and remove all the reservations for the current bookID
     Node *bookNode = redBlackTree->search(bookID);
     if (bookNode == nullptr) {
         cout << "Book not found in the Library" << endl << endl;
         return;
     }
     cout << "Book " << bookID << " is no longer available. ";
-    if(!bookNode->minHeap->heap.empty()) {
+    if (!bookNode->minHeap->heap.empty()) {
         // At least Someone has borrowed the book
         string reservations = bookNode->minHeap->heap.size() > 1 ? "Reservations" : "Reservation";
         string patrons = bookNode->minHeap->heap.size() > 1 ? "patrons " : "patron ";
         string hasHave = bookNode->minHeap->heap.size() > 1 ? " have" : " has";
         cout << reservations << " made by " << patrons;
-        for (int i = 0; i < (int)bookNode->minHeap->heap.size() - 1; i++) {
+        for (int i = 0; i < (int) bookNode->minHeap->heap.size() - 1; i++) {
             cout << bookNode->minHeap->heap[i]->userID << ", ";
         }
         cout << bookNode->minHeap->heap.back()->userID << hasHave << " been cancelled!" << endl << endl;
@@ -151,14 +165,17 @@ void GatorLibrary::DeleteBook(int bookID) {
 }
 
 void GatorLibrary::FindClosestBook(int bookID) const {
+    // Find the closest book to the given bookID
     if (redBlackTree->root == nullptr) {
         cout << "Library is empty" << endl;
         return;
     }
     if (redBlackTree->search(bookID) != nullptr) {
+        // Book is present in the library, hence directly print the book and return
         PrintBook(bookID);
         return;
     }
+    // Fetch the list of books and compare both the ends of the books with the bookID. If difference is same, print both in the order
     vector<Node *> books;
     redBlackTree->inorderTraversal(redBlackTree->root, books, -1e6, 1e6);
     int idx = findSmallestGreater(books, bookID);
@@ -168,6 +185,7 @@ void GatorLibrary::FindClosestBook(int bookID) const {
         PrintBook(books.back()->book->bookID);
     } else {
         if (bookID - books[idx - 1]->book->bookID == books[idx]->book->bookID - bookID) {
+            // Same difference
             PrintBook(books[idx - 1]->book->bookID);
             PrintBook(books[idx]->book->bookID);
         } else if (bookID - books[idx - 1]->book->bookID < books[idx]->book->bookID - bookID) {
@@ -179,10 +197,12 @@ void GatorLibrary::FindClosestBook(int bookID) const {
 }
 
 void GatorLibrary::ColorFlipCount() const {
+    // Print the color flip count
     cout << "Colour Flip Count : " << flipCount << endl << endl;
 }
 
 void GatorLibrary::Quit() {
+    // Quit the program
     cout << "Program Terminated!!" << endl;
     exit(0);
 }
